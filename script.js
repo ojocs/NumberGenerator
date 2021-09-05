4134d581b1ca6f1d37d4cabdded2429eda25adb8
Osvaldo Jimenez <osvcs40@gmail.com>
2021-09-05 15:07:44 -0500

    Make output inclusive of max value

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function randomNum(){
     min = document.getElementById("min").value;
     max = document.getElementById("max").value;
     number = Math.random() * (max - min) + min;
-    number = Math.floor(number);
+    number = Math.random() < 0.5 ? Math.floor(number) : Math.ceil(number);
     if(number == null){
         number = "Bad Input";
     }
